84aa4155e98c0905b408808f8097601f0f2cabd3
Fabi019 <fabi019@gmx.de>
2023-12-01 19:24:07 +0100

    refactor day 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,15 +1,15 @@
-use std::collections::BTreeMap;
-
 static INPUT: &str = include_str!("../../assets/day01.txt");
 
 fn main() {
     let now = std::time::Instant::now();
-    println!("Part 1: {}", part1(INPUT));
+    let s = part1(INPUT); // 54951
     println!("Took: {:?}", now.elapsed());
+    println!("Part 1: {}", s);
 
     let now = std::time::Instant::now();
-    println!("Part 2: {}", part2(INPUT));
+    let s = part2(INPUT); // 55218
     println!("Took: {:?}", now.elapsed());
+    println!("Part 2: {}", s);
 }
 
 fn part1(input: &str) -> u32 {
@@ -24,38 +24,39 @@ fn part1(input: &str) -> u32 {
 }
 
 fn part2(input: &str) -> u32 {
-    const NUMBER_STRINGS: [&str; 10] = [
-        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    const NUMBER_STRINGS: [&str; 9] = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
 
-    input.lines().fold(0, |acc, line| {
-        let mut numbers = BTreeMap::new();
-
-        // Insert digits as they are
-        for (pos, c) in line.char_indices() {
-            if let Some(v) = c.to_digit(10) {
-                numbers.insert(pos, v);
-            }
-        }
-
-        // Convert words to digits
-        for (value, word) in NUMBER_STRINGS.iter().enumerate() {
-            for (pos, _) in line.match_indices(word) {
-                numbers.insert(pos, value as u32);
-            }
-        }
-
-        let numbers = numbers.values().cloned().collect::<Vec<_>>();
-        let (first, last) = (numbers[0], numbers[numbers.len() - 1]);
-        acc + first * 10 + last
-    })
+    input
+        .lines()
+        .map(|l| {
+            l.char_indices().filter_map(|(pos, c)| {
+                if let Some(v) = c.to_digit(10) {
+                    Some(v)
+                } else {
+                    for (value, word) in NUMBER_STRINGS.iter().enumerate() {
+                        if l[pos..].starts_with(word) {
+                            return Some(value as u32 + 1);
+                        }
+                    }
+                    None
+                }
+            })
+        })
+        .fold(0, |acc, mut numbers| {
+            let first = numbers.next().unwrap();
+            let last = numbers.last().unwrap_or(first);
+            acc + first * 10 + last
+        })
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const EXAMPLE1: &str = r#"1abc2
+    const EXAMPLE1: &str = r#"\
+1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet"#;
@@ -65,7 +66,8 @@ treb7uchet"#;
         assert_eq!(part1(EXAMPLE1), 142);
     }
 
-    const EXAMPLE2: &str = r#"two1nine
+    const EXAMPLE2: &str = r#"\
+two1nine
 eightwothree
 abcone2threexyz
 xtwone3four
