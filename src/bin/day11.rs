c5ee25cbd170edad6d14f7349128be21f77b684a
Fabi019 <fabi019@gmx.de>
2023-12-11 19:36:46 +0100

    refactor day 11

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,45 +1,39 @@
 aoc2023::main!("../../assets/day11.txt");
 
-fn part1(input: &str) -> u64 {
+fn part1(input: &str) -> usize {
     let universe = input
         .lines()
         .map(|line| line.chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
-    calculate(universe, 2)
+    calculate(&universe, 2)
 }
 
-fn part2(input: &str) -> u64 {
+fn part2(input: &str) -> usize {
     let universe = input
         .lines()
         .map(|line| line.chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
     let factor = if cfg!(test) { 100 } else { 1000000 };
-    calculate(universe, factor)
+    calculate(&universe, factor)
 }
 
-fn calculate(universe: Vec<Vec<char>>, factor: usize) -> u64 {
-    // Collect all galaxies
+fn calculate(universe: &[Vec<char>], factor: usize) -> usize {
+    // Collect all galaxies and apply horizontal offset if a line is empty
     let mut galaxies = Vec::new();
+    let mut offset = 0;
+    #[allow(clippy::needless_range_loop)]
     for y in 0..universe.len() {
+        let mut galaxy_in_line = false;
         for x in 0..universe[y].len() {
             if universe[y][x] == '#' {
-                galaxies.push((x, y));
+                galaxies.push((x, y + offset));
+                galaxy_in_line = true;
             }
         }
-    }
-
-    // Apply a horizontal offset when a line is empty
-    for y in (0..universe.len()).rev() {
-        if universe[y].iter().any(|c| *c != '.') {
-            continue;
-        }
-
-        for galaxy in &mut galaxies {
-            if galaxy.1 > y {
-                galaxy.1 += factor - 1;
-            }
+        if !galaxy_in_line {
+            offset += factor - 1;
         }
     }
 
@@ -60,8 +54,8 @@ fn calculate(universe: Vec<Vec<char>>, factor: usize) -> u64 {
     let mut path_sum = 0;
     for i in 0..galaxies.len() {
         for j in i + 1..galaxies.len() {
-            let dx = (galaxies[i].0 as i64).abs_diff(galaxies[j].0 as i64);
-            let dy = (galaxies[i].1 as i64).abs_diff(galaxies[j].1 as i64);
+            let dx = galaxies[i].0.abs_diff(galaxies[j].0);
+            let dy = galaxies[i].1.abs_diff(galaxies[j].1);
             path_sum += dx + dy;
         }
     }
